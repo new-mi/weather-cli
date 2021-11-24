396b38313175f77ef8572175ade063cdd7f5cd81
Mikhail Vaginl <mvagin@bk.ru>
2021-11-24 04:19:00 +0300

    Вывод в консоль

---
+++ services/log.js
@@ -0,0 +1,17 @@
+import chalk from 'chalk'
+import dedent from 'dedent-js'
+
+const { bgCyan, bgRed } = chalk
+
+export const printError = (errorText) => console.log(`${bgRed(' ERROR ')} ${errorText}`);
+
+export const printSuccess = (successText) => console.log(`${bgGreen(' SUCCESS ')} ${successText}`);
+
+export const printHelp = () => console.log(
+	dedent`${bgCyan(' HELP ')}
+	Без параметров	вывод погоды
+	-s [CITY]	установка города
+	-h		вывод помощи
+	-t [API_KEY]	сохранение токена
+	`
+);
