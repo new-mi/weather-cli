91c97d5c5a64ac429b3efa92f83462b5a078a586
Mikhail Vaginl <mvagin@bk.ru>
2021-11-24 15:19:40 +0300

    OS and path

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printHelp } from "./services/log.js";
+import { saveKeyValue } from "./services/storage.js";
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
@@ -11,7 +12,7 @@ const initCLI = () => {
 		// Сохранить город
 	}
 	if (args.t) {
-		// Сохранить token
+		saveKeyValue('token', args.t)
 	}
 	// Вывести погоду
 }
